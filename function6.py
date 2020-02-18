dc134199688b761fe30dd2916f5228e8b438d660
Kaekaefx <48446517+Kaekaefx@users.noreply.github.com>
2020-02-18 13:18:28 +0200

    updated comments.

--- function6.py
+++ function6.py
@@ -2,5 +2,5 @@ def word_splitter(df):
     """This function returns dataframe with a 'Split Tweets' column containing a list of 'words' (all lowercase) from each tweet,
     this function accepts one dataframe as an argument"""
     
-    df['Split Tweets'] = [string.lower().split() for string in df['Tweets']] #Splitting the tweet string and assigning the entries to the Tweets column
+    df['Split Tweets'] = [string.lower().split() for string in df['Tweets']] #Splitting each tweet string and assigning the entries to the Tweets column
     return df
