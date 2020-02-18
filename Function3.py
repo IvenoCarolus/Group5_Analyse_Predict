1790f203ea408594d704e8c6cfd501c1be9d8e1d
NicoleMeinie <60350495+NicoleMeinie@users.noreply.github.com>
2020-02-18 09:28:44 +0200

    created function 3

---
+++ Function3.py
@@ -0,0 +1,5 @@
+def date_pharser(date):  
+   
+    #This function takes a list of datetime strings and converts it into a list of strings with only the date.
+    
+    return  [item[:10] for item in date]
