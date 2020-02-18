9c0a4a0be8267c3e59f61d8fe4ac768f0ee50b80
Kaekaefx <48446517+Kaekaefx@users.noreply.github.com>
2020-02-18 13:20:43 +0200

    fixed typo in docstring and comments

--- function5.py
+++ function5.py
@@ -1,7 +1,7 @@
 def number_of_tweets_per_day(df):
-    """Function returns data frame containing column for numbre of tweets per day and Date, takes one dataframe as arg"""
+    """Function returns data frame containing column for numbre of tweets per day and Date, takes one dataframe as an argument."""
    
-    df['Date'] = [item[:10] for item in df['Date']]  #extracting the date by removing the timestamp
+    df['Date'] = [item[:10] for item in df['Date']]  #extracting the date by slicing up until the first 10 characters in the date string.
     df['Tweets'] = [1 for item in df['Tweets']]     #replacing each tweet with the number 1.
     
-    return df.groupby('Date').sum() #grouping the tweets according to the date and summing the number of tweets per date
+    return df.groupby('Date').sum() #grouping the tweets according to the date and summing the number of tweets per date.
