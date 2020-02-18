e10d52cca3ce5bdf94897d3001ee989a14ce7f93
Kaekaefx <48446517+Kaekaefx@users.noreply.github.com>
2020-02-18 13:11:59 +0200

    adding docstrings and comments to function6.

--- function6.py
+++ function6.py
@@ -1,3 +1,5 @@
-def word_splitter(df):  
-    df['Split Tweets'] = [string.lower().split() for string in df['Tweets']]
-    return df 
+def word_splitter(df):
+    """Function returns dataframe with Split Tweets column which contains a list of 'words' in every tweet, string must also be lowercase, one dataframe as arg"""
+    
+    df['Split Tweets'] = [string.lower().split() for string in df['Tweets']] #simply just splitting tweet string using str.split() and then assigning the entries to Tweets column
+    return df
