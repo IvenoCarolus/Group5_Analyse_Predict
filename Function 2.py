ac944ead2dcf0c56e2da02f8d598366deb2e5d22
NicoleMeinie <60350495+NicoleMeinie@users.noreply.github.com>
2020-02-18 11:00:04 +0200

    Added a doc string and updated inline comments

--- Function 2.py
+++ Function 2.py
@@ -1,14 +1,14 @@
-
-#Five number summary
 def five_num_summary(items):
-""""These function creates a dictionary of five number summary""""
-    mx = np.max(items)
-    q2 = np.median(items)
-    mn = np.min(items)
-    q1 = np.percentile(items,25)
-    q3 = np.percentile(items,75)
-# Insert the variables within a dictionary.
-    dic =  {'max': mx, 'median': q2, 'min': mn, 'q1': q1, 'q3': q3}
-#Return the dictionary.
-    return dic
+""""This function takes in a list of integers and returns a dictionary of the five number summary
+     values are rounded to 2 decimal places""""
+
+    mx = np.max(items)                   # maximum value
+    q2 = np.median(items)                # median value
+    mn = np.min(items)                   # minumum value 
+    q1 = np.percentile(items,25)         # first quartile  
+    q3 = np.percentile(items,75)         # third quartile  
+    
+    return  {'max': mx, 'median': q2, 'min': mn, 'q1': q1, 'q3': q3} #return the five number summary in a dictionary 
+
+    
 
