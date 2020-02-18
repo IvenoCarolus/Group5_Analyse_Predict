139fd8c0b35561bab64640755e17e19061ba826f
mandla <60220361+0731325603@users.noreply.github.com>
2020-02-18 10:33:59 +0200

    Update Function 2.py

--- Function 2.py
+++ Function 2.py
@@ -1,13 +1,13 @@
 
 #Five number summary
 def five_num_summary(items):
-   
+""""These function creates a dictionary of five number summary""""
     mx = np.max(items)
     q2 = np.median(items)
     mn = np.min(items)
     q1 = np.percentile(items,25)
     q3 = np.percentile(items,75)
- # Insert the variables within a dictionary.
+# Insert the variables within a dictionary.
     dic =  {'max': mx, 'median': q2, 'min': mn, 'q1': q1, 'q3': q3}
 #Return the dictionary.
     return dic
