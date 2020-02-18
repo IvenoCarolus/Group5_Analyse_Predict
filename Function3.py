69cbbfbc6708b038c36f5d5f8d7fafbbee79d7f1
Kaekaefx <48446517+Kaekaefx@users.noreply.github.com>
2020-02-18 13:25:44 +0200

    fixed typo in function definition
    And changed block comment to inline comment

--- Function3.py
+++ Function3.py
@@ -1,5 +1,4 @@
-def date_pharser(date):     
+def date_parser(date):     
     """This function takes a list of datetime strings and converts it into a list of strings with only the date."""
     
-   #create a new list containing strings of only the first 10 characters of the original strings, i.e the date only.
-    return [item[:10] for item in date]  
+    return [item[:10] for item in date]  #create a new list containing strings of only the first 10 characters of the original strings, i.e the date only.
