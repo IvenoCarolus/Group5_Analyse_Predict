a22290fba83c486ed1b6c69313f5a4fd3847f5e7
NicoleMeinie <60350495+NicoleMeinie@users.noreply.github.com>
2020-02-18 10:07:56 +0200

    Added comments and a doc string

--- Function3.py
+++ Function3.py
@@ -1,5 +1,5 @@
-def date_pharser(date):  
-   
-    #This function takes a list of datetime strings and converts it into a list of strings with only the date.
+def date_pharser(date):     
+    """This function takes a list of datetime strings and converts it into a list of strings with only the date."""
     
-    return  [item[:10] for item in date]
+   #create a new list containing strings of only the first 10 characters of the original strings, i.e the date only.
+    return [item[:10] for item in date]  
