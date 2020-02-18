13d4d06e575d4eb59269035feaf4f83bd9cb231a
Kaekaefx <48446517+Kaekaefx@users.noreply.github.com>
2020-02-18 14:48:14 +0200

    Delete Function3.py

--- Function3.py
+++
@@ -1,4 +0,0 @@
-def date_parser(date):     
-    """This function takes a list of datetime strings and converts it into a list of strings with only the date."""
-    
-    return [item[:10] for item in date]  #create a new list containing strings of only the first 10 characters of the original strings, i.e the date only.
