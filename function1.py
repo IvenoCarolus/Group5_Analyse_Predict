45851034ab3f00bd0f9865b0fddc574281df4175
mandla <60220361+0731325603@users.noreply.github.com>
2020-02-18 10:18:17 +0200

    Add files via upload
    Metric dictionary

---
+++ function1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Function for data metrics
+def dictionary_of_metrics(items):
+    
+    return {'mean':round(np.mean(items),2),
+            'median':np.median(items), 
+            'var':round(np.var(items,ddof=1),2),
+            'std':round(np.std(items,ddof=1),2),
+            'min':np.min(items),
+            'max':np.max(items)}
+
