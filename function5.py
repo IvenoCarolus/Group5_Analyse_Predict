2d4fa337da7097943aaabbe3f2fa50d6ac15eefd
NicoleMeinie <60350495+NicoleMeinie@users.noreply.github.com>
2020-02-18 13:06:36 +0200

    changed block comments to inline comments

--- function5.py
+++ function5.py
@@ -1,11 +1,7 @@
 def number_of_tweets_per_day(df):
     """Function returns data frame containing column for numbre of tweets per day and Date, takes one dataframe as arg"""
+   
+    df['Date'] = [item[:10] for item in df['Date']]  #extracting the date by removing the timestamp
+    df['Tweets'] = [1 for item in df['Tweets']]     #replacing each tweet with the number 1.
     
-    #extracting date - removing timestamp
-    df['Date'] = [item[:10] for item in df['Date']]
-    
-    #replacing each tweet with 1.
-    df['Tweets'] = [1 for item in df['Tweets']]
-    
-    #then returning the sum
-    return df.groupby('Date').sum()
+    return df.groupby('Date').sum() #grouping the tweets according to the date and summing the number of tweets per date
